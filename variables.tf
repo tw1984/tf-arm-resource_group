0d36be9edae0672306e6aef09063fb4b34d38805
tw1984 <woodlandt@gmail.com>
2020-01-25 21:07:45 -0800

    changed naming module url

--- variables.tf
+++ variables.tf
@@ -42,6 +42,6 @@ locals {
 
 # This module provides a data map output to lookup naming standard references
 module "naming" {
-  source = "git::https://github.com/CLEAResult/cr-azurerm-naming.git?ref=v1.1.0"
+  source = "git::https://github.com/tw1984/tf-arm-naming.git?ref=v0.5"
 }
 
