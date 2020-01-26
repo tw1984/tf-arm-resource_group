9ef04b5a4b3f33582a6650889048069af63b8547
tw1984 <woodlandt@gmail.com>
2020-01-25 21:07:22 -0800

    removed azuread group outputs

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,3 @@
 output "id" {
   value = azurerm_resource_group.rg.id
 }
-
-output "readerId" {
-  value = azuread_group.reader.*.id
-}
-
-output "readerName" {
-  value = azuread_group.reader.*.name
-}
-
