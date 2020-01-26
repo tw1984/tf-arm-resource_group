1fbbce39a86753cc7c728418fe2337028a10ebef
tw1984 <woodlandt@gmail.com>
2020-01-25 21:06:54 -0800

    removed azuread group creation

--- main.tf
+++ main.tf
@@ -14,14 +14,3 @@ resource "azurerm_resource_group" "rg" {
     InfrastructureAsCode = "True"
   }
 }
-
-resource "azuread_group" "reader" {
-  name = format("g%s%s%s_AZ_Reader", local.default_rgid, local.env_id, local.rg_type)
-}
-
-resource "azurerm_role_assignment" "reader" {
-  scope                = format("%s/resourceGroups/%s", data.azurerm_subscription.primary.id, azurerm_resource_group.rg.name)
-  role_definition_name = "Reader"
-  principal_id         = azuread_group.reader.id
-}
-
